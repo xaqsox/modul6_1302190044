18250a49421cd8fb0e90b6dd86503638709fc966
Mochamad Aqsa <101115008+xaqsox@users.noreply.github.com>
2023-03-27 07:50:19 +0700

    add class SayaTubeVideo dan SayaTubeUser
    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        id = random.Next(10000, 100000);
+        this.title = title;
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+  
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video Details");
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
---
+++ SayaTubeUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302190044
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private string username;
+        List<SayaTubeVideo> uploadedVideos = new List<SayaTubeVideo>();
+        public SayaTubeUser Data { get; set; }
+        public SayaTubeUser() {
+            
+           // for (int i = 0;i < id; i++)
+           // {
+            //    Console.WriteLine(id);
+           // }
+            this.username = username;
+        }
+        public int GetTotalVideoPlay()
+        {
+            return GetTotalVideoPlay();
+        }
+        public void AddVideo()
+        {
+            SayaTubeUser ts = new SayaTubeUser();
+            ts.GetTotalVideoPlay();
+        }
+        public void PrintAllVideoPlaycount()
+        {
+
+        }
+    }
+}
